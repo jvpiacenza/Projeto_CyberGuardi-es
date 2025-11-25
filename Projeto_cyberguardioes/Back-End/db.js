35fc27191fee0541f3732ad7c56bbe53846a2f63
João Victor Piacenza de Oliveira Andrade <joaovictorpiacenza@gmail.com>
2025-11-25 12:56:21 -0300

    Add files via upload

---
+++ Projeto_cyberguardioes/Back-End/db.js
@@ -0,0 +1,17 @@
+
+const mysql = require('mysql2/promise')
+
+
+const pool = mysql.createPool({
+
+    host:"localhost",
+    user:"root", 
+    password:"",
+    port:3306,
+    database:"banco_projeto",
+    waitForConnections:true, 
+    connectionLimit:10, 
+    queueLimit:0 
+})
+
+module.exports = pool 
